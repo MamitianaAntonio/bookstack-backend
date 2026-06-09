58a468a89e3dfc3a88e7ff23412f7411ad17a982
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-06-09 17:43:53 +0300

    feat: add Book and BookRepository entities imports

--- src/main/java/com/bookstack/backend/repository/BookCopyRepository.java
+++ src/main/java/com/bookstack/backend/repository/BookCopyRepository.java
@@ -1,14 +1,18 @@
 package com.bookstack.backend.repository;
 
+import com.bookstack.backend.entity.BookCopy;
+import com.bookstack.backend.enums.Format;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.UUID;
 
-public interface BookCopyRepository extends JpaRepository<BookCopy, UUID>{
+public interface BookCopyRepository extends JpaRepository<BookCopy, UUID> {
     List<BookCopy> findByBookId(UUID bookId);
-    List<BookCopy> findByFormat(BookCopyFormat format);
+    List<BookCopy> findByFormat(Format format);
     List<BookCopy> findByPriceLessThanEqual(BigDecimal maxPrice);
 
     @Query("SELECT bc FROM BookCopy bc WHERE bc.price BETWEEN :min AND :max")
--- src/main/java/com/bookstack/backend/repository/BookRepository.java
+++ src/main/java/com/bookstack/backend/repository/BookRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.bookstack.backend.entity.Book;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
