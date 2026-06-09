11444c23fdde26782ecadcdddf0d0e1f05f0e3d8
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-06-09 23:08:21 +0300

    fix: remove files causing CI/CD pipeline to fail

--- src/main/java/com/bookstack/backend/repository/GenreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bookstack.backend.repository;
-
-import java.util.UUID;
-
-public interface GenreRepository extends JpaRepository<Genre, UUID> {
-  Optional<Genre> findByName(String name);
-}
--- src/main/java/com/bookstack/backend/repository/LibraryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bookstack.backend.repository;
-
-import java.util.UUID;
-
-public interface LibraryRepository extends JpaRepository<Library, UUID> {
-  List<Library> findByName(String name);
-}
