6c27154fdada652b28d551f2762946907c21c6cb
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-06-10 19:10:35 +0300

    feat: add multi-criteria search method in BookRepository

--- src/main/java/com/bookstack/backend/repository/BookRepository.java
+++ src/main/java/com/bookstack/backend/repository/BookRepository.java
@@ -22,4 +22,18 @@ public interface BookRepository extends JpaRepository<Book, String> {
 
   @Query("SELECT b FROM Book b WHERE b.title ILIKE %:keyword% OR b.summary ILIKE %:keyword%")
   List<Book> findByKeyword(@Param("keyword") String keyword);
+
+  @Query(
+      """
+                  SELECT b FROM Book b
+                  LEFT JOIN b.authors a
+                  LEFT JOIN b.genres g
+                  WHERE (:authorId IS NULL OR a.id = :authorId)
+                  AND (:genreId IS NULL OR g.id = :genreId)
+                  AND (:title IS NULL OR b.title ILIKE %:title%)
+      """)
+  List<Book> findByCriteria(
+      @Param("authorId") String authorId,
+      @Param("genreId") String genreId,
+      @Param("title") String title);
 }
