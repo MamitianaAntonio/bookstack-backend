0b801e25428638a9272ecd97094e8e4de2795885
Diarilala <hei.diarilala@gmail.com>
2026-06-12 21:29:40 +0300

    fix: add data to entity JPriceHistory

--- src/main/java/com/bookstack/backend/repository/model/JPriceHistory.java
+++ src/main/java/com/bookstack/backend/repository/model/JPriceHistory.java
@@ -1,12 +1,26 @@
 package com.bookstack.backend.repository.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.Instant;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
 
 @Table(name = "price_history")
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class JPriceHistory {}
+public class JPriceHistory {
+  @Id @UuidGenerator private String id;
+
+  @Column(nullable = false)
+  private BigDecimal price;
+
+  @Column(nullable = false, name = "changed_at")
+  private Instant changedAt;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "book_copy_id", nullable = false)
+  private JBookCopy bookCopy;
+}
