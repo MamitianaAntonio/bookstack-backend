102342653a2a7cf6dc641e04f7ef1a0876398c11
Diarilala <hei.diarilala@gmail.com>
2026-06-12 21:23:16 +0300

    fix: add entity to missing JPriceHistory

--- src/main/java/com/bookstack/backend/repository/model/JPriceHistory.java
+++ src/main/java/com/bookstack/backend/repository/model/JPriceHistory.java
@@ -1,3 +1,12 @@
 package com.bookstack.backend.repository.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Table(name = "price_history")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class JPriceHistory {}
