698c63663f325ad2963d784afaad0dc6ff0502a5
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-06-14 23:03:55 +0300

    style(repository): improve formatting of getTotalRevenue query in SaleRepository

--- src/main/java/com/bookstack/backend/repository/SaleRepository.java
+++ src/main/java/com/bookstack/backend/repository/SaleRepository.java
@@ -18,12 +18,11 @@ public interface SaleRepository extends JpaRepository<Sale, String> {
   List<Sale> findByStatus(Status status);
 
   @Query(
-      """
-      SELECT SUM(bc.price) FROM Sale s JOIN s.
-      bookCopies bc
-      WHERE s.status ='COMPLETED'
-      AND s.
-      saleDate BETWEEN :
-      start AND :end""")
+      """              
+      SELECT SUM(bc.price) FROM Sale s
+      JOIN s.bookCopies bc
+      WHERE s.status = 'COMPLETED'
+      AND s.saleDate BETWEEN :start AND :end
+      """)
   BigDecimal getTotalRevenue(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 }
