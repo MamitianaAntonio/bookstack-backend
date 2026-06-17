7c103f324a23176073e6fafd5e9603438038210d
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-06-17 23:17:34 +0300

    feat: create mappers for Author, Book, and Genre

--- src/main/java/com/bookstack/backend/mapper/GenreMapper.java
+++ src/main/java/com/bookstack/backend/mapper/GenreMapper.java
@@ -2,35 +2,34 @@ package com.bookstack.backend.mapper;
 
 import com.bookstack.backend.model.Genre;
 import com.bookstack.backend.repository.model.JGenre;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @AllArgsConstructor
 public class GenreMapper {
-    public List<Genre> toModel(List<JGenre> genres) {
-        return genres.stream().map(this::toModel).toList();
-    }
+  public List<Genre> toModel(List<JGenre> genres) {
+    return genres.stream().map(this::toModel).toList();
+  }
 
-    public Genre toModel(JGenre jGenre) {
-        Genre genre = new Genre();
-        genre.setId(jGenre.getId());
-        genre.setName(jGenre.getName());
-        genre.setDescription(jGenre.getDescription());
-        return genre;
-    }
+  public Genre toModel(JGenre jGenre) {
+    Genre genre = new Genre();
+    genre.setId(jGenre.getId());
+    genre.setName(jGenre.getName());
+    genre.setDescription(jGenre.getDescription());
+    return genre;
+  }
 
-    public List<JGenre> toEntity(List<Genre> genres) {
-        return genres.stream().map(this::toEntity).toList();
-    }
+  public List<JGenre> toEntity(List<Genre> genres) {
+    return genres.stream().map(this::toEntity).toList();
+  }
 
-    public JGenre toEntity(Genre genre) {
-        JGenre jGenre = new JGenre();
-        jGenre.setId(genre.getId());
-        jGenre.setName(genre.getName());
-        jGenre.setDescription(genre.getDescription());
-        return jGenre;
-    }
+  public JGenre toEntity(Genre genre) {
+    JGenre jGenre = new JGenre();
+    jGenre.setId(genre.getId());
+    jGenre.setName(genre.getName());
+    jGenre.setDescription(genre.getDescription());
+    return jGenre;
+  }
 }
