6625a98a1ab5a3f9fccc4543035eba44cc0e74aa
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-06-17 23:19:25 +0300

    style: format BookController and BookService

--- src/main/java/com/bookstack/backend/endpoint/rest/controller/health/BookController.java
+++ src/main/java/com/bookstack/backend/endpoint/rest/controller/health/BookController.java
@@ -2,38 +2,37 @@ package com.bookstack.backend.endpoint.rest.controller.health;
 
 import com.bookstack.backend.model.Book;
 import com.bookstack.backend.service.BookService;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
 public class BookController {
-    private final BookService service;
+  private final BookService service;
 
-    @GetMapping("/books")
-    public List<Book> getAll() {
-        return service.findAll();
-    }
+  @GetMapping("/books")
+  public List<Book> getAll() {
+    return service.findAll();
+  }
 
-    @GetMapping("/books/{id}")
-    public Book getById(@PathVariable String id) {
-        return service.findById(id);
-    }
+  @GetMapping("/books/{id}")
+  public Book getById(@PathVariable String id) {
+    return service.findById(id);
+  }
 
-    @PostMapping("/books")
-    public List<Book> create(@RequestBody List<Book> toSave) {
-        return service.create(toSave);
-    }
+  @PostMapping("/books")
+  public List<Book> create(@RequestBody List<Book> toSave) {
+    return service.create(toSave);
+  }
 
-    @PutMapping("/books/{id}")
-    public Book update(@PathVariable String id, @RequestBody Book book) {
-        return service.update(id, book);
-    }
+  @PutMapping("/books/{id}")
+  public Book update(@PathVariable String id, @RequestBody Book book) {
+    return service.update(id, book);
+  }
 
-    @DeleteMapping("/books/{id}")
-    public void delete(@PathVariable String id) {
-        service.delete(id);
-    }
+  @DeleteMapping("/books/{id}")
+  public void delete(@PathVariable String id) {
+    service.delete(id);
+  }
 }
