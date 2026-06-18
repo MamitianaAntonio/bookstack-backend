934d65daf29dd681ebf4bcb7b4cac4581112c148
Diarilala <hei.diarilala@gmail.com>
2026-06-18 15:21:55 +0300

    feat: add AuthorController basic CRUD

--- src/main/java/com/bookstack/backend/endpoint/rest/controller/health/AuthorController.java
+++ src/main/java/com/bookstack/backend/endpoint/rest/controller/health/AuthorController.java
@@ -4,8 +4,7 @@ import com.bookstack.backend.model.Author;
 import com.bookstack.backend.service.AuthorService;
 import java.util.List;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -13,5 +12,32 @@ public class AuthorController {
   private AuthorService authorService;
 
   @GetMapping("/authors")
-  public List<Author> getAuthors() {}
+  public List<Author> getAuthors() {
+    return authorService.findAll();
+  }
+
+  @GetMapping("/authors/{id}")
+  public Author getAuthorById(@PathVariable String id) {
+    return authorService.findById(id);
+  }
+
+  @PostMapping("/author")
+  public Author createAuthor(@RequestBody Author author) {
+    return authorService.create(author);
+  }
+
+  @PostMapping("/authors")
+  public List<Author> createAuthors(@RequestBody List<Author> authors) {
+    return authorService.createList(authors);
+  }
+
+  @PutMapping("/authors/{id}")
+  public Author updateAuthor(@PathVariable String id, @RequestBody Author author) {
+    return authorService.update(id, author);
+  }
+
+  @DeleteMapping("/authors/{id}")
+  public void deleteAuthor(@PathVariable String id) {
+    authorService.delete(id);
+  }
 }
