e10df3bf701eee9854329d63f2be66b50eb14fd3
Diarilala <hei.diarilala@gmail.com>
2026-06-18 15:08:27 +0300

    feat: add AuthorService basic CRUD

--- src/main/java/com/bookstack/backend/endpoint/rest/controller/health/AuthorController.java
+++ src/main/java/com/bookstack/backend/endpoint/rest/controller/health/AuthorController.java
@@ -2,17 +2,16 @@ package com.bookstack.backend.endpoint.rest.controller.health;
 
 import com.bookstack.backend.model.Author;
 import com.bookstack.backend.service.AuthorService;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @AllArgsConstructor
 public class AuthorController {
-    private AuthorService authorService;
+  private AuthorService authorService;
 
-    @GetMapping("/authors")
-    public List<Author> getAuthors() {}
+  @GetMapping("/authors")
+  public List<Author> getAuthors() {}
 }
--- src/main/java/com/bookstack/backend/service/AuthorService.java
+++ src/main/java/com/bookstack/backend/service/AuthorService.java
@@ -3,53 +3,47 @@ package com.bookstack.backend.service;
 import com.bookstack.backend.mapper.AuthorMapper;
 import com.bookstack.backend.model.Author;
 import com.bookstack.backend.repository.AuthorRepository;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class AuthorService {
-    private final AuthorRepository repository;
-    private final AuthorMapper mapper;
+  private final AuthorRepository repository;
+  private final AuthorMapper mapper;
 
-    public List<Author> findAll() {
-        return mapper.toModel(repository.findAll());
-    }
+  public List<Author> findAll() {
+    return mapper.toModel(repository.findAll());
+  }
 
-    public Author findById(String id) {
-        return mapper.toModel(
-                repository
-                        .findById(id)
-                        .orElseThrow(() -> new RuntimeException("Author with id " + id + " not found"))
-        );
-    }
-
-    public List<Author> createList(List<Author> toSave) {
-        return mapper.toModel(
-                repository.saveAll(mapper.toEntity(toSave))
-        );
-    }
-
-    public Author create(Author toSave) {
-        return mapper.toModel(
-                repository.save(mapper.toEntity(toSave))
-        );
-    }
-
-    public Author update(String id, Author author) {
+  public Author findById(String id) {
+    return mapper.toModel(
         repository
-                .findById(id)
-                .orElseThrow(() -> new RuntimeException("Author with id " + id + " not found"));
-        author.setId(id);
-        return mapper.toModel(repository.save(mapper.toEntity(author)));
-    }
+            .findById(id)
+            .orElseThrow(() -> new RuntimeException("Author with id " + id + " not found")));
+  }
 
-    public void delete(String id) {
-        repository
-                .findById(id)
-                .orElseThrow(() -> new RuntimeException("Author with id " + id + " not found"));
-        repository.deleteById(id);
-    }
+  public List<Author> createList(List<Author> toSave) {
+    return mapper.toModel(repository.saveAll(mapper.toEntity(toSave)));
+  }
+
+  public Author create(Author toSave) {
+    return mapper.toModel(repository.save(mapper.toEntity(toSave)));
+  }
+
+  public Author update(String id, Author author) {
+    repository
+        .findById(id)
+        .orElseThrow(() -> new RuntimeException("Author with id " + id + " not found"));
+    author.setId(id);
+    return mapper.toModel(repository.save(mapper.toEntity(author)));
+  }
+
+  public void delete(String id) {
+    repository
+        .findById(id)
+        .orElseThrow(() -> new RuntimeException("Author with id " + id + " not found"));
+    repository.deleteById(id);
+  }
 }
