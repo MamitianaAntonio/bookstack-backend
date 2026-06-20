270f88c68e4de11cfbac8489dd00da63e84e1f9f
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-06-20 20:15:52 +0300

    refactor: move all exception files to /exception and leave only ApiException in /exception/model

--- src/main/java/com/bookstack/backend/exception/model/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.bookstack.backend.exception.model;
-
-import org.springframework.http.HttpStatus;
-
-public class BadRequestException extends ApiException {
-    public BadRequestException(String message) {
-        super(message, HttpStatus.BAD_REQUEST);
-    }
-}
--- src/main/java/com/bookstack/backend/exception/model/ExceptionBody.java
+++
@@ -1,6 +0,0 @@
-package com.bookstack.backend.exception.model;
-
-import java.time.Instant;
-
-public record ExceptionBody(int status, String error, String message, String path, Instant timestamp) {
-}
