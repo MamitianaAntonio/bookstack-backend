3bd3f28667221df539af3a407a7727202f5c8b15
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-06-20 20:02:27 +0300

    feat: add ForbiddenException

---
+++ src/main/java/com/bookstack/backend/exception/model/ForbiddenException.java
@@ -0,0 +1,9 @@
+package com.bookstack.backend.exception.model;
+
+import org.springframework.http.HttpStatus;
+
+public class ForbiddenException extends ApiException {
+    public ForbiddenException(String message) {
+        super(message, HttpStatus.FORBIDDEN);
+    }
+}
