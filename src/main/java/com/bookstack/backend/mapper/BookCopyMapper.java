326bf981b2039272f8a6b275ddc8814209153347
Lavorary <hei.lavorary@gmail.com>
2026-06-21 21:14:06 +0300

    feat : create BookCopyMapper

---
+++ src/main/java/com/bookstack/backend/mapper/BookCopyMapper.java
@@ -0,0 +1,53 @@
+package com.bookstack.backend.mapper;
+
+import com.bookstack.backend.model.BookCopy;
+import com.bookstack.backend.repository.model.JBookCopy;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+public class BookCopyMapper {
+    private final BookMapper bookMapper;
+    private final PublisherMapper publisherMapper;
+
+    public List<BookCopy> toModel(List<JBookCopy> bookCopies) {
+        return bookCopies.stream().map(this::toModel).toList();
+    }
+
+    public BookCopy toModel(JBookCopy jBookCopy) {
+        BookCopy bookCopy = new BookCopy();
+        bookCopy.setId(jBookCopy.getId());
+        bookCopy.setFormat(jBookCopy.getFormat());
+        bookCopy.setLanguage(jBookCopy.getLanguage());
+        if (jBookCopy.getBook() != null) {
+            bookCopy.setBook(bookMapper.toModel(jBookCopy.getBook()));
+        }
+        if (jBookCopy.getPublisher() != null) {
+            bookCopy.setPublisher(publisherMapper.toModel(jBookCopy.getPublisher()));
+        }
+
+        return bookCopy;
+    }
+
+    public List<JBookCopy> toEntity(List<BookCopy> bookCopies) {
+        return bookCopies.stream().map(this::toEntity).toList();
+    }
+
+    public JBookCopy toEntity(BookCopy bookCopy) {
+        JBookCopy bookCopyEntity = new JBookCopy();
+        bookCopyEntity.setId(bookCopy.getId());
+        bookCopyEntity.setFormat(bookCopy.getFormat());
+        bookCopyEntity.setLanguage(bookCopy.getLanguage());
+        if (bookCopy.getBook() != null) {
+            bookCopyEntity.setBook(bookMapper.toEntity(bookCopy.getBook()));
+        }
+        if (bookCopy.getPublisher() != null) {
+            bookCopyEntity.setPublisher(publisherMapper.toEntity(bookCopy.getPublisher()));
+        }
+
+        return bookCopyEntity;
+    }
+}
