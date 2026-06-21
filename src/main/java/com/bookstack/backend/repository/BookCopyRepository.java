4faf2bcfc20efb14ab12af5417c9e19dd5b8b5b4
Lavorary <hei.lavorary@gmail.com>
2026-06-21 21:27:41 +0300

    chore : add findByPublisherId in BookCopyRepository

--- src/main/java/com/bookstack/backend/repository/BookCopyRepository.java
+++ src/main/java/com/bookstack/backend/repository/BookCopyRepository.java
@@ -21,4 +21,6 @@ public interface BookCopyRepository extends JpaRepository<JBookCopy, String> {
   @Query(
       "SELECT bc FROM JBookCopy bc JOIN bc.priceHistories ph WHERE ph.price BETWEEN :min AND :max")
   List<JBookCopy> findByPriceRange(@Param("min") BigDecimal min, @Param("max") BigDecimal max);
+
+  List<JBookCopy> findByPublisherId(String publisherId);
 }
