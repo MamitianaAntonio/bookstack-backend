3249e6847b8ba8dafbd3365f7e7269c7bdfb08e8
Lavorary <hei.lavorary@gmail.com>
2026-07-07 23:44:58 +0300

    feat : add FormatStockDTO

---
+++ src/main/java/com/bookstack/backend/dto/FormatStockDTO.java
@@ -0,0 +1,17 @@
+package com.bookstack.backend.dto;
+
+import com.bookstack.backend.enums.Format;
+import com.bookstack.backend.enums.Language;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FormatStockDTO {
+    private String bookCopyId;
+    private Format format;
+    private Language language;
+    private Integer stock;
+}
