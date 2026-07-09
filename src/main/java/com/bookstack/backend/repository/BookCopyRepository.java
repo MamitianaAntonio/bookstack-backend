f64eaba88ba1d5af302a74e4b7ce10cdba77f051
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-07-09 18:24:48 +0300

    feat: add sql query to get all bookCopyIdsWithLowStock

--- src/main/java/com/bookstack/backend/repository/BookCopyRepository.java
+++ src/main/java/com/bookstack/backend/repository/BookCopyRepository.java
@@ -43,4 +43,13 @@ public interface BookCopyRepository extends JpaRepository<JBookCopy, String> {
         WHERE bc.book.id = :bookId
       """)
   Integer calculateTotalStockByBookId(@Param("bookId") String bookId);
+
+  @Query("""
+    SELECT bc.id FROM JBookCopy bc
+    LEFT JOIN bc.arrivalItems ai
+    LEFT JOIN bc.saleItems si
+    GROUP BY bc.id
+    HAVING COALESCE(SUM(ai.quantity), 0) - COALESCE(SUM(si.quantity), 0) <= :threshold
+    """)
+  List<String> findBookCopyIdsWithLowStock(@Param("threshold") int threshold);
 }
