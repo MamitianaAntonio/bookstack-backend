b01532f9fb310d09169975548e2d80f3503be3f9
Finoana Randrianasolo <finoanarandrianasolo3@gmail.com>
2026-07-15 19:56:32 +0300

    style: remove unused import

--- src/test/java/com/bookstack/backend/endpointControllertest/BookControllerTest.java
+++ src/test/java/com/bookstack/backend/endpointControllertest/BookControllerTest.java
@@ -2,7 +2,6 @@ package com.bookstack.backend.endpointControllertest;
 
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import com.bookstack.backend.dto.BookInfoDTO;
 import com.bookstack.backend.dto.request.BookRequestDTO;
